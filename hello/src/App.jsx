67c0ea14ee8e2a064daa79b9e06fcd0f6d739c3c
GUILHERME ZAMBONI MENEGACIO <guilherme_menegacio@estudante.sesisenai.org.br>
2024-08-19 17:07:45 -0300

    Adicionado hello

---
+++ hello/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <h1 className='titulo1'>Hello React JS Fofinho</h1>
+    
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+      Totam voluptates adipisci ullam et veniampariatur ex, recusandae 
+      quam dignissimos distinctio consectetur soluta aliquam architecto 
+      deserunt maiores tempora odit doloribus quae.;</p>
+    </>
+  )
+}
+
+export default App
